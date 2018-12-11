739b2946f19f688f1b68bef22e74a8183d5b94ee
alumno Secundaria <al3201@cedi.local>
2018-12-11 09:10:41 -0600

    Proyecto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProductoPageModule } from '../producto/producto.module';
+import { ProductoPage } from '../producto/producto';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  prod = ProductoPage;
+
+  Productos = [
+    {id: 0,color: "white", Producto: "Zapatos de Futbol",precio: "$1500", Vendedor: "Edson Gonzalez",valoracion: [1, 2, 3, 4, 5],imagen: "../assets/imgs/ZapatosdeFutbol.jpg", numero: "(871) 178 5027", resena: [{usuario:"Daniel de la Torre", fecha: "07/12/2018",  avatar: "../assets/imgs/Hafro", texto:"" }]},
+    {id: 1,color: "white", Producto: "Camisa Deportiva",precio: "$600",Vendedor: "Edson Gonzalez",valoracion: [1, 2, 3, 4 ],imagen: "../assets/imgs/Camisa.jpg", numero: "(871) 178 5027"},
+    {id: 2,color: "white", Producto: "Mochila deportiva",precio: "$500",Vendedor: "Edson Gonzalez",valoracion: [1, 2, 3,],imagen: "../assets/imgs/Mochila.jfif", numero: "(871) 178 5027"},
+    {id: 3,color: "white", Producto: "Espinilleras",precio: "$200",Vendedor: "Edson Gonzalez",valoracion: [1, 2, 3, 4, 5],imagen: "../assets/imgs/Espinilleras.jpg", numero: "(871) 178 5027"},
+  ];
+
   constructor(public navCtrl: NavController) {
 
-  }
+    }
+
+    clickProducto(p){
+      this.navCtrl.push(this.prod, {producto: p});
+    }
+
 
 }
---
+++ src/pages/producto/producto.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ProductoPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-producto',
+  templateUrl: 'producto.html',
+})
+export class ProductoPage {
+
+producto;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.producto = this.navParams.get("producto");
+
+  }
+  
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ProductoPage');
+  }
+
+}
